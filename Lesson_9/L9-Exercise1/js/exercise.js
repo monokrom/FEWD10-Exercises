af8402ee632a8c492c4ad7105a42cce5e5123a74
MIchael Shippard <michaelshippard@MacBook-Pro.local>
2016-02-15 20:58:35 +1100

    Lesson 9 end

--- Lesson_9/L9-Exercise1/js/exercise.js
+++ Lesson_9/L9-Exercise1/js/exercise.js
@@ -8,7 +8,10 @@
  * Question 1:
  * When, button 1 is clicked, select all paragraphs on the page with jQuery, and changed the color to blue.
  */
- 
+ $('#button1').click(function(){
+     
+     $('p').css('color', 'blue');
+ });
 
 
 /**
@@ -16,13 +19,21 @@
  * When button 1 is clicked, select all <h2> elements on this page, and change the text to say your name.
  */
 
-
+$('#button1').click(function(){
+    
+    $('h2').text('Michael');
+})
 
 /**
  * Question 3:
  * When button 1 is clicked, select the ordered list item that has the class "falseFact". 
  * Using jQuery change the content to read "True Fact".
  */
+
+$('#button1').click(function(){
+    
+    $('.falseFact').text('True Fact');
+});
 
 
 
@@ -31,22 +42,32 @@
  * Question 4: 
  * When button 2 is clicked, change the background color of the whole page to "pink"
  */
+$('#button2').click(function(){
+    
+   $('body').css('background-color', 'pink');
 
-
-
+});
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
 
+$('#button2').click(function(){
+    
+   $('h2').css('color', 'green');
 
+});
 
 /**
  * Question 6:
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
+$('#button2').click(function(){
+    
+   $('blockquote').html('<span>no quote</span>');
 
+});
 
 
 
@@ -56,7 +77,11 @@
  * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
  */
 
+$('#button3').click(function(){
+    
+   $('h1').text('jQuery Ninja');
 
+});
 
 
 
@@ -68,12 +93,28 @@
 
 
 
+$('#button3').click(function(){
+    
+  $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+alert('I changed the image!');
+
+});
+
 /**
  * Question 9:
  * When button 3 is clicked, select all paragraphs on the page and change the color to blue and font to Georgia.
  * Bonus: Try to do this with only one jQuery call. $("selector").css({attribute: "value", attribute: "value"});
  */
 
+
+$('#button3').click(function(){
+    
+  $('p').css({
+      color: 'blue', 
+      fontFamily: 'Georgia'
+  });
+
+});
 
 
 
